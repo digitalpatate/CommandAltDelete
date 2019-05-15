c8f2f1a27c4b6b7999e2745790eea50856809d2f
Didier Page <page.didier@gmail.com>
2019-05-15 17:22:00 +0200

    <feat> Ajout de javafx

--- src/main/java/ch/heigvd/thecommandmasters/Main.java
+++ src/main/java/ch/heigvd/thecommandmasters/Main.java
@@ -1,7 +1,62 @@
 package ch.heigvd.thecommandmasters;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
 
-public class Main {
+
+public class Main extends Application{
+
+    public Main() {
+        super();
+    }
+
+    @Override
+    public void init() throws Exception {
+        super.init();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle( "Canvas Example" );
+
+        Group root = new Group();
+        Scene theScene = new Scene( root );
+        stage.setScene( theScene );
+
+        Canvas canvas = new Canvas( 400, 200 );
+        root.getChildren().add( canvas );
+
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        gc.setFill( Color.RED );
+        gc.setStroke( Color.BLACK );
+        gc.setLineWidth(2);
+        Font theFont = Font.font( "Times New Roman", FontWeight.BOLD, 48 );
+        gc.setFont( theFont );
+        gc.fillText( "Hello, World!", 60, 50 );
+        gc.strokeText( "Hello, World!", 60, 50 );
+
+        Image earth = new Image( "earth.jpg" );
+        gc.drawImage( earth, 180, 100 );
+
+        stage.show();
+    }
+
     static void main(String...args){
+
+        launch(args);
 
     }
 }
