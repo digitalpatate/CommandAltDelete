726616ab75e27de33d097cc2325850f730a89bd2
andy <andres.moreno@heig-vd.ch>
2019-06-05 16:04:56 +0200

    added commentaries to map

--- src/main/java/ch/heigvd/thecommandmasters/Game/Map.java
+++ src/main/java/ch/heigvd/thecommandmasters/Game/Map.java
@@ -2,32 +2,47 @@ package ch.heigvd.thecommandmasters.Game;
 
 import ch.heigvd.thecommandmasters.Character.Entity;
 
+/***
+ * Class representing the map logic of the game
+ */
+
 public class Map {
 
     private Entity stage[];
-    private Entity p1,p2;
+    private Entity p1, p2;
     private int mapSize;
 
 
+    /***
+     * Constructor of Map
+     *
+     * @param mapSize the size of the map (need to consider only one dimension)
+     * @param p1 the first player
+     * @param p2 the second player
+     */
+    public Map(int mapSize, Entity p1, Entity p2) {
 
-public Map(int mapSize, Entity p1, Entity p2){
+        this.mapSize = mapSize;
+        stage = new Entity[mapSize];
+        this.p1 = p1;
+        this.p2 = p2;
 
-    this.mapSize=mapSize;
-    stage= new Entity[mapSize];
-    this.p1=p1;
-    this.p2=p2;
+        p1.setPosition(0);
+        p2.setPosition(mapSize - 1);
 
-    p1.setPosition(0);
-    p2.setPosition(mapSize-1);
-
-    stage[p1.getPosition()]=p1;
-    stage[p2.getPosition()]=p2;
+        stage[p1.getPosition()] = p1;
+        stage[p2.getPosition()] = p2;
 
 
+    }
 
-}
 
-
+    /***
+     * Method enabling to move a player at the map
+     *
+     * @param player the player that will perform the movement
+     * @param movement the amount of cases that the player will move
+     */
     public void move(Entity player, int movement) {
 
         int id = player.getId();
@@ -37,6 +52,12 @@ public Map(int mapSize, Entity p1, Entity p2){
 
     }
 
+    /***
+     *
+     * @param player the player that will perform the movement
+     * @param movement the amount of cases that the player will move
+     * @param id the id of the player
+     */
     private void displacement(Entity player, int movement, int id) {
         if (id == 0) {
 
@@ -49,32 +70,42 @@ public Map(int mapSize, Entity p1, Entity p2){
         }
     }
 
+    /***
+     *
+     * @param player the player that will perform the movement
+     * @param movement the amount of cases that the player will move
+     */
     private void playerTwoMove(Entity player, int movement) {
         if (movement > 0) {
 
-                if (player.getPosition() - movement > p1.getPosition()) {
+            if (player.getPosition() - movement > p1.getPosition()) {
 
-                    player.move(movement);
-
-                } else {
-
-                    player.setPosition(p1.getPosition() + 1);
-
-                }
+                player.move(movement);
 
             } else {
 
-                if (player.getPosition() - movement <= mapSize - 1) {
+                player.setPosition(p1.getPosition() + 1);
 
-                    player.move(movement);
+            }
 
-                } else {
-                    player.setPosition(mapSize - 1);
+        } else {
 
-                }
+            if (player.getPosition() - movement <= mapSize - 1) {
+
+                player.move(movement);
+
+            } else {
+                player.setPosition(mapSize - 1);
+
+            }
         }
     }
 
+    /***
+     *
+     * @param player the player that will perform the movement
+     * @param movement the amount of cases that the player will move
+     */
     private void playerOneMove(Entity player, int movement) {
         if (movement > 0) {
 
@@ -103,7 +134,6 @@ public Map(int mapSize, Entity p1, Entity p2){
 
         }
     }
-
 
 
 }
