f6744460ad0eca54695188abecea39e9b8cb2475
Nohan Budry <nohan.budry@icloud.com>
2019-06-05 17:09:35 +0200

    ActionInvoker can undo last action

--- src/main/java/ch/heigvd/thecommandmasters/command/invoker/ActionInvoker.java
+++ src/main/java/ch/heigvd/thecommandmasters/command/invoker/ActionInvoker.java
@@ -2,12 +2,21 @@ package ch.heigvd.thecommandmasters.command.invoker;
 
 import ch.heigvd.thecommandmasters.command.Action;
 
+import java.util.LinkedList;
+
 public class ActionInvoker {
 
     private ActionInvokerSlave[] invokers;
+    private int[] invocationOrder;
 
     public ActionInvoker(int playerCount) {
         this.invokers = new ActionInvokerSlave[playerCount];
+        this.invocationOrder = new int[playerCount];
+
+        // Set a default order
+        for (int i = 0; i < playerCount; ++i) {
+            invocationOrder[i] = playerCount - i - 1;
+        }
     }
 
     public void setEntityActions(int entityId, Action[] actions) {
@@ -43,32 +52,87 @@ public class ActionInvoker {
             return;
         }
 
+        int id = -1;
         int i = 0;
-        ActionInvokerSlave invoker = null;
 
-        // Get the first invoker slave with actions
+        // Get first invoker with actions
         for (; i < invokers.length; ++i) {
             if (invokers[i].hasNext()) {
-                invoker = invokers[i];
+                id = i;
                 break;
             }
         }
 
-        if (invoker != null) {
-            for (;i < invokers.length; ++i) {
+        if (id >= 0) {
 
-                // Ignore invokers without actions
-                if (invokers[i].hasNext()) {
-
-                    // TODO what to do if equal priority?
-
-                    if (invokers[i].getPriority() < invoker.getPriority()) {
-                        invoker = invokers[i];
-                    }
+            // Get invoker with lowest priority and least recently used
+            ++i;
+            for (; i < invokers.length; ++i) {
+                int compare = Integer.compare(invokers[i].getPriority(), invokers[id].getPriority());
+                if (compare == 0) {
+                    id = getLeastRecentlyUsedId(id, i);
+                } else if (compare < 0) {
+                    id = i;
                 }
             }
 
-            invoker.invokeNext();
+            updateInvocationOrder(id);
+            invokers[id].invokeNext();
         }
+    }
+
+    public void undoOpponentLast(int entityId) {
+
+        if (!checkEntityId(entityId)) {
+            return;
+        }
+
+        for (int i : invocationOrder) {
+            if (i != entityId) {
+                invokers[i].undoPrevious();
+            }
+        }
+    }
+
+    private void updateInvocationOrder(int entityId) {
+
+        if (!checkEntityId(entityId)) {
+            return;
+        }
+
+        int currentId = entityId;
+        int tmp;
+
+        for (int i = 0; i < invokers.length; ++i) {
+
+            tmp = invocationOrder[i];
+            invocationOrder[i] = currentId;
+            currentId = tmp;
+
+            if (currentId == entityId) {
+                return;
+            }
+        }
+    }
+
+    private int getLeastRecentlyUsedId(int a, int b) {
+
+        for (int i = invokers.length - 1; i >= 0; --i) {
+            if (invocationOrder[i] == a) {
+                return a;
+            } else if (invocationOrder[i] == b) {
+                return b;
+            }
+        }
+
+        return a;
+    }
+
+    private boolean checkEntityId(int id) {
+        return id >= 0 && id < invokers.length;
+    }
+
+    int[] getInvocationOrder() {
+        return invocationOrder;
     }
 }
