7f043d78918fa1ce1a94330f86a1f643cda13b49
Nohan Budry <nohan.budry@icloud.com>
2019-06-10 22:12:57 +0200

    temporary fix: passing wrong commands to playRound

--- src/main/java/ch/heigvd/thecommandmasters/Scene/Game/menu/EndTurnPanel.java
+++ src/main/java/ch/heigvd/thecommandmasters/Scene/Game/menu/EndTurnPanel.java
@@ -1,8 +1,11 @@
 package ch.heigvd.thecommandmasters.Scene.Game.menu;
 
+import ch.heigvd.thecommandmasters.command.Command;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.util.LinkedList;
 
 import static ch.heigvd.thecommandmasters.state.game.GameContext.*;
 
@@ -18,7 +21,10 @@ public class EndTurnPanel extends JPanel {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 System.out.println("End turn");
-                getInsance().getSelectedCommands().add(getInsance().getGameScene().getSelectedCommandPanel().getCommands());
+                // TODO find better way than clone!
+                getInsance().getSelectedCommands().add(
+                        (LinkedList<Command>) getInsance().getGameScene().getSelectedCommandPanel().getCommands().clone()
+                );
                 getInsance().setState(getInsance().getState().getNextState());
             }
         });
